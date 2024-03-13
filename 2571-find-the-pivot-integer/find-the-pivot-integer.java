8c8bb980c2a4cde60087f99beef925050bfb2029
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2024-03-13 18:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (17.72%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -2,9 +2,9 @@ class Solution {
     public int pivotInteger(int n) {
       
         double x2= (n*(n+1))/2;
-        int x=  (int)Math.sqrt(x2);
-        
-        if(x*x==x2)
+        double sr=  Math.sqrt(x2);
+        int x= (int)Math.round(sr);
+        if(sr==x)
         return x;
         else
         return -1;
