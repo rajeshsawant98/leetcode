ed66a2c9ce5e3d6322c26502d5038ed6f34dfd7d
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2024-04-07 21:54:35 +0530

    Time: 1 ms (99.39%) | Memory: 45.1 MB (40.81%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+
+        List<String> list= new ArrayList<>();
+        int i=1;
+        while(i<=n){
+            if(i%3==0 && i%5==0){
+                list.add("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                list.add("Fizz");
+            }
+            else if(i%5==0)
+            {
+                list.add("Buzz");
+            }
+            else{
+                list.add(Integer.toString(i));
+            }
+            i++;
+
+        }
+
+        return list;
+    }
+}
