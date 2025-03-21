b9dcffa16491e664dba5e7ee3d61b511d7336b01
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2025-03-21 15:27:02 -0700

    Time: 15 ms (26.80%) | Memory: 12.4 MB (84.33%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        map ={}
+        map["I"]= 1
+        map["V"]= 5
+        map["X"]= 10
+        map["L"]=50
+        map["C"]=100
+        map["D"]=500
+        map["M"]=1000
+
+        ans=0
+        prev=0
+
+        for c in reversed(s):
+            value = map[c]
+            if value < prev:
+                ans -= value
+            if value >= prev:
+                ans += value
+            prev = value
+        return ans
+            
