15373b3d0b6354ed9292c6b5218975b321940180
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2025-03-21 15:31:34 -0700

    Time: 14 ms (29.75%) | Memory: 12.6 MB (13.25%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -5,14 +5,16 @@ class Solution(object):
         :rtype: int
         """
         
-        map ={}
-        map["I"]= 1
-        map["V"]= 5
-        map["X"]= 10
-        map["L"]=50
-        map["C"]=100
-        map["D"]=500
-        map["M"]=1000
+        map ={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        
 
         ans=0
         prev=0
