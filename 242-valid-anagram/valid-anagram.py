45845acb80c3b5c9adf06a6aa5630ac33c7b52c9
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2025-03-23 14:21:16 -0700

    Time: 19 ms (68.83%) | Memory: 14.5 MB (10.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -6,4 +6,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s)==Counter(t)
+
+        if len(s)!= len(t): 
+            return False
+
+
+        Cs ={}
+        Ct ={}
+
+        for i in range(len(s)):
+            Cs[s[i]]= Cs.get(s[i],0)+1
+            Ct[t[i]]= Ct.get(t[i],0)+1
+        
+        return Cs == Ct
