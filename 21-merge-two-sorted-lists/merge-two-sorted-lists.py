b9a27bfe98bbcaa6ac703099fa24be97fdda6b5e
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2025-03-23 18:06:42 -0700

    Time: 4 ms (13.06%) | Memory: 12.6 MB (7.24%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -11,22 +11,22 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        dummy = ListNode()
-        ans = dummy
-
+        ans =ListNode()
+        head = ans
 
         while list1 and list2:
             if list1.val <= list2.val:
-                ans.next = list1
+                head.next = list1
                 list1= list1.next
-            else: 
-                ans.next = list2
+            else:
+                head.next = list2
                 list2 = list2.next
-            ans = ans.next
-        
+            head = head.next
+
         if list1:
-            ans.next = list1
+            head.next = list1
+        
         if list2:
-            ans.next = list2
-            
-        return dummy.next
+            head.next = list2
+        
+        return ans.next
