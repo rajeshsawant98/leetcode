087cf4cd21a46ed83a67e627bb4f480740607602
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2025-03-25 14:30:55 -0700

    Time: 551 ms (37.78%) | Memory: 21.5 MB (98.41%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -6,53 +6,53 @@ class Solution(object):
         :rtype: int
         """
 
-        # if not nums:
-        #     return 0
+        if not nums:
+            return 0
 
-        # nums.sort()
+        nums.sort()
 
-        # L=0 
-        # R=len(nums)-1
+        L=0 
+        R=len(nums)-1
 
-        # c=0
+        c=0
 
-        # while(L<R):
-        #     sum = nums[L]+ nums[R]
-        #     if sum == k:
-        #         c +=1
-        #         R -=1
-        #         L +=1
-        #     elif sum > k:
-        #         R -=1
-        #     else:
-        #         L +=1
+        while(L<R):
+            sum = nums[L]+ nums[R]
+            if sum == k:
+                c +=1
+                R -=1
+                L +=1
+            elif sum > k:
+                R -=1
+            else:
+                L +=1
         
-        # return c
-
-        cmap = {}
-        c = 0
-
-        for i in nums:
-            cmap[i] = cmap.get(i, 0) + 1
-
-        for n in nums:
-            diff = k - n
-            if n == diff:
-                if cmap.get(n, 0) >= 2:
-                    c += 1
-                    cmap[n] -= 2
-                    if cmap[n] <= 0:
-                        cmap.pop(n)
-            elif cmap.get(n, 0) > 0 and cmap.get(diff, 0) > 0:
-                c += 1
-                cmap[n] -= 1
-                cmap[diff] -= 1
-                if cmap[n] == 0:
-                    cmap.pop(n)
-                if cmap[diff] == 0:
-                    cmap.pop(diff)
-
         return c
+
+        # cmap = {}
+        # c = 0
+
+        # for i in nums:
+        #     cmap[i] = cmap.get(i, 0) + 1
+
+        # for n in nums:
+        #     diff = k - n
+        #     if n == diff:
+        #         if cmap.get(n, 0) >= 2:
+        #             c += 1
+        #             cmap[n] -= 2
+        #             if cmap[n] <= 0:
+        #                 cmap.pop(n)
+        #     elif cmap.get(n, 0) > 0 and cmap.get(diff, 0) > 0:
+        #         c += 1
+        #         cmap[n] -= 1
+        #         cmap[diff] -= 1
+        #         if cmap[n] == 0:
+        #             cmap.pop(n)
+        #         if cmap[diff] == 0:
+        #             cmap.pop(diff)
+
+        # return c
 
         
 
