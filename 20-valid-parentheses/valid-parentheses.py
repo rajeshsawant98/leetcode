b2f5cb61e3b7c4338b80753cfbb6fa5fd2420dd9
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2025-03-30 19:35:25 -0700

    Time: 3 ms (50.74%) | Memory: 12.6 MB (57.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,19 +5,40 @@ class Solution(object):
         :rtype: bool
         """
         
+        # stack =[]
+        # ClosedBracMap = { ']': '[','}':'{',')':'('}
+
+        # for c in s:
+        #     if c in ClosedBracMap: 
+        #         if stack and stack[-1]==ClosedBracMap[c]:
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         stack.append(c)
+            
+        # return True if not stack else False
+                
+            
+        
         stack =[]
-        ClosedBracMap = { ']': '[','}':'{',')':'('}
 
         for c in s:
-            if c in ClosedBracMap: 
-                if stack and stack[-1]==ClosedBracMap[c]:
-                    stack.pop()
+            if c == ')' or c == ']' or c == '}': 
+                if stack:
+                    if c== ')' and stack[-1]=='(':
+                        stack.pop()
+                    elif c== ']' and stack[-1]=='[':
+                        stack.pop()
+                    elif c== '}' and stack[-1]=='{':
+                        stack.pop()
+                    else:
+                        return False
                 else:
                     return False
             else:
                 stack.append(c)
             
         return True if not stack else False
-                    
 
 
