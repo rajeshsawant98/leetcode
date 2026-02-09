510ffe96168eec8ebca71ee0bfa95e9d43ef7e29
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-02-08 19:11:46 -0700

    Time: 0 ms (100.00%) | Memory: 13.5 MB (40.65%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -6,15 +6,16 @@ class Solution(object):
         :rtype: int
         """
         
-        l,r = 0 ,len(nums)-1
-
-        while(l<=r):
-            m= (r+l)// 2
-            if target < nums[m]:
-                r = m - 1
-            elif target > nums[m]:
-                l = m + 1
-            else:
+        l,r=0,len(nums)-1
+        
+        for i in range(len(nums)):
+            m= l+ (r-l)//2
+            if nums[m]==target:
                 return m
+            elif nums[m]< target:
+                l= m+1
+            elif nums[m] > target:
+                r= m-1
         
         return -1
+            
