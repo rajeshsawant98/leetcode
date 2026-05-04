b128ff29db68822b50e6f848456adfac17f4c98a
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-05-04 11:54:37 -0700

    Time: 555 ms (56.90%) | Memory: 12.4 MB (81.59%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+
+        count = 0
+
+        for i in range(len(nums)-1, 1, -1 ):
+            l = 0 
+            r = i-1
+            while (l<r):
+                if nums[l] + nums[r] > nums[i]:
+                    count += r - l
+                    r -=1 
+                else:
+                    l +=1
+            
+        return count
