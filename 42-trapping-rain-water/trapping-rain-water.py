adbea339b6dfdc6c76e11f8e465755cba62afc04
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-05-12 18:13:58 -0700

    Time: 3 ms (97.93%) | Memory: 13.5 MB (58.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,24 +4,26 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        if not height :
+            return 0
         
-        l,r= 0 , len(height)-1
-        lmax,rmax= height[l],height[r]
-        res=0
+        l,r = 0, len(height)-1
+        lmax , rmax = height[l],height[r]
+        count = 0
 
         while(l<r):
-            
-            if lmax < rmax:
+            if(lmax <= rmax):
                 l += 1
-                lmax = max (lmax, height[l])
-                res += lmax - height[l]
-              
+                if( height[l] >= lmax):
+                    lmax = height[l]
+                else:
+                    count += lmax - height[l]
             else:
                 r -= 1
-                rmax = max (rmax, height[r])
-                res += rmax - height[r]
-               
+                if ( height[r] >= rmax):
+                    rmax = height[r]
+                else:
+                    count += rmax - height[r]
+
         
-        return res
-
-
+        return count
