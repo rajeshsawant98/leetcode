e364e2f9e3142061fbecf533f6aca66874adc639
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-05-27 20:35:28 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (27.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,44 +1,16 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
-        # stack =[]
-        # ClosedBracMap = { ']': '[','}':'{',')':'('}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
 
-        # for c in s:
-        #     if c in ClosedBracMap: 
-        #         if stack and stack[-1]==ClosedBracMap[c]:
-        #             stack.pop()
-        #         else:
-        #             return False
-        #     else:
-        #         stack.append(c)
-            
-        # return True if not stack else False
-                
-            
-        
-        stack =[]
+        closedBrackets = { "]" : "[" , "}" : "{" , ")" : "("}
 
         for c in s:
-            if c == ')' or c == ']' or c == '}': 
-                if stack:
-                    if c== ')' and stack[-1]=='(':
-                        stack.pop()
-                    elif c== ']' and stack[-1]=='[':
-                        stack.pop()
-                    elif c== '}' and stack[-1]=='{':
-                        stack.pop()
-                    else:
-                        return False
+            if c in closedBrackets:
+                if stack and stack[-1] == closedBrackets[c]:
+                    stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
-            
-        return True if not stack else False
-
-
+        
+        return not stack
