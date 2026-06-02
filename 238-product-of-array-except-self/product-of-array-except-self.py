c2c90534211c66d374fbd55f9414b527c09be6ae
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-06-02 12:32:03 -0700

    Time: 29 ms (72.63%) | Memory: 20.3 MB (37.07%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,32 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        pre=[]
+        res= [1]* len(nums)
 
-        preProduct = 1
-
+        prefix = 1
         for i in range(len(nums)):
-            preProduct *= nums[i]
-            pre.append(preProduct)
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
         
-        post=[]
-
-        postProduct = 1
-
-        for j in range(len(nums)-1,-1,-1):
-            postProduct *= nums[j]
-            post.append(postProduct) 
-
-        post = post[::-1]
-
-        for i in range(len(nums)):
-            if i == 0:
-                nums[i] = post[i+1]
-            elif i == len(nums)-1:
-                nums[i] = pre[i-1]
-            else:
-                nums[i] = pre[i-1]*post[i+1] 
-
-        return nums 
-
-        
+        return res
