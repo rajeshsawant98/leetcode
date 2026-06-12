224e527f60f4771e0475574a48417425568fe216
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-06-12 16:46:58 -0700

    Time: 11 ms (87.40%) | Memory: 12.6 MB (52.09%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        res =[]
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        board = [["."]*n for r in range(n)]
+
+        def backtrack(r):
+            if r == n :
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in cols or (r-c) in negDiag or (r+c) in posDiag:
+                    continue
+                
+                cols.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+                board[r][c] ="Q"
+                backtrack(r+1)
+
+                cols.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+                board[r][c] ="."
+            
+        backtrack(0)
+
+        return res
