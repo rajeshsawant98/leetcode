baf9046e9832e907ca8420f0bfec55b0f462d99a
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-07-27 21:59:00 -0700

    Time: 15 ms (41.24%) | Memory: 23.8 MB (26.06%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,26 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # #brute sort all the strings and group the same ones that would take n*nlogn 
+
+        # res = defaultdict(list)
+
+        # for s in strs:
+        #     sortedS = ''.join(sorted(s))
+        #     res[sortedS].append(s)
         
+        # return list(res.values())
+
         res = defaultdict(list)
 
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c)- ord('a')] +=1
-            
+        for s in strs:  # n
+            count = [0]*26 # to count the occurence of each chara in the string
+            for c in s: # 
+                count[ord(c) - ord("a")] += 1 
             res[tuple(count)].append(s)
         
         return list(res.values())
+
+        # O(n.k)
+        # O(n.k)
+
+            
