f45a3902da296b41895de702463fa1c87d8ede6c
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-07-30 13:56:24 -0700

    Time: 123 ms (55.67%) | Memory: 78.8 MB (16.62%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,48 +1,56 @@
 class Node:
-    def __init__(self,key:int, value:int):
-        self.val = value
-        self.next = self.prev = None
-        self.key = key
-
+    def __init__(self, key: int, val: int):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
-        self.left , self.right = Node(0,0), Node(0,0)
-        self.left.next , self.right.prev = self.right , self.left
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
     
-    def remove(self, node):
-        prev , nxt = node.prev, node.next
-        prev.next ,nxt.prev = nxt, prev
+    def insert(self, Node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = Node
+        Node.next, Node.prev = nxt, prev
     
-    def insert(self, node):
-        prev , nxt = self.right.prev , self.right
-        prev.next = nxt.prev = node
-        node.next , node.prev = nxt, prev
-
-        
+    def delete(self, Node):
+        prev, nxt = Node.prev, Node.next
+        prev.next, nxt.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.delete(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val 
-        return -1
+
+            return self.cache[key].val
         
+        return -1
 
     def put(self, key: int, value: int) -> None:
 
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.delete(self.cache[key])
         self.cache[key] = Node(key,value)
         self.insert(self.cache[key])
 
         if len(self.cache) > self.cap:
             lru = self.left.next
-            self.remove(lru)
+            self.delete(lru)
             del self.cache[lru.key]
+
+"""
+
+cache
+
+ : (1,1)
+4 : (4,4)
+
+L ->  (3,3) -> (4,4) -> R
+"""
         
 
 
