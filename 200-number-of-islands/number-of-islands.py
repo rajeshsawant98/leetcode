cc749845b069a19621f9a89e1e99ed2dced970b2
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-01 13:12:49 -0700

    Time: 265 ms (21.98%) | Memory: 26.8 MB (16.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,36 +1,42 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows,cols = len(grid), len(grid[0])
-        visit =set()
-        res= 0
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
 
         def bfs(r,c):
-            
-            q = collections.deque()
-
+            q = deque()
             q.append((r,c))
-            visit.add((r, c))
-
-            directions = [(1,0),(-1,0),(0,1),(0,-1)]
+            visit.add((r,c))
 
             while q:
-                row,col = q.popleft()
+                qlen = len(q)
 
-                for dr,dc in directions:
-                    r,c = row+dr,col+dc
+                directions = [(0,1),(1,0),(0,-1),(-1,0)]
 
-                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visit:
-                        q.append((r,c))
-                        visit.add((r,c))
-        
+                for _ in range(qlen):
+                    row,col = q.popleft()
+
+                    for dr,dc in directions:
+                        r,c = row + dr , col + dc
+
+                        if r >= 0 and r < rows and c >= 0 and c < cols and (r,c) not in visit and grid[r][c] == "1" :
+                            q.append((r,c))
+                            visit.add((r,c))
+                    
 
 
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visit:
-                    res += 1
                     bfs(r,c)
-        
+                    islands +=1
+                else:
+                    continue
 
-        return res
+        
+        return islands
+        
