4b493982f809e2672c176e8701ee6ca67aaf453c
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-02 11:30:58 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (95.92%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -7,19 +7,23 @@ class Solution:
 
             if c != "]":
                 stack.append(c)
+            
             else:
 
-                substring =""
+                substring = ""
+
                 while stack and stack[-1] != "[":
                     substring = stack.pop() + substring
                 stack.pop()
-
+                
                 multiplier = ""
+
                 while stack and stack[-1].isdigit():
                     multiplier = stack.pop() + multiplier
                 
-                stack.append(int(multiplier)* substring)
-                
+                stack.append(int(multiplier) * substring)
+        
 
         return "".join(stack)
+                
 
