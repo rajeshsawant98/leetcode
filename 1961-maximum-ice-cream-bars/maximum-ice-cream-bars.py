30d17bf30e02b85efcf142450883aefc5b3d4f0c
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-06 15:17:57 -0700

    Time: 33 ms (96.08%) | Memory: 31 MB (71.68%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -1,18 +1,44 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
 
-        count = 0
-        minHeap = costs
-        heapq.heapify(costs)
-        while coins > 0 and minHeap:
-            if minHeap[0] <= coins:
-                count += 1
-                cost = heapq.heappop(minHeap)
-                coins -= cost
-            else:
-                break
+        # count = 0
+        # minHeap = costs
+        # heapq.heapify(costs)
+        # while coins > 0 and minHeap:
+        #     if minHeap[0] <= coins:
+        #         count += 1
+        #         cost = heapq.heappop(minHeap)
+        #         coins -= cost
+        #     else:
+        #         break
         
-        return count
+        # return count
+
+        m = max(costs)
+        icecreams = 0
+
+        Count = [0] * (m + 1)
+
+        for cost in costs:
+            Count[cost] += 1
+
+        for cost in range(1, m + 1) :
+
+            if Count[cost] == 0:
+                continue
+
+            if coins < cost:
+                break
+            
+            count = min(Count[cost], coins// cost)
+
+            coins -= count * cost
+
+            icecreams += count
+        
+        return icecreams
+
+
 
 
 
