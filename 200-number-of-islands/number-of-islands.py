ddda3267e0828b75445592ef8a1e2441885a9ac3
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-07 10:47:50 -0700

    Time: 266 ms (21.22%) | Memory: 26.8 MB (20.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -16,18 +16,17 @@ class Solution:
             directions = [ (1,0), (0,1), (-1,0), (0,-1)]
 
             while q:
-                qlen = len(q)
-                for _ in range(qlen):
-                    row, col = q.popleft()
 
-                    for dr, dc in directions:
-                        r,c = row + dr, col + dc
+                row, col = q.popleft()
 
-                        if r<0 or r >= rows or c <0 or c >= cols or grid[r][c] == "0" or (r,c) in visit:
-                            continue
-                        
-                        visit.add((r,c))
-                        q.append((r,c))
+                for dr, dc in directions:
+                    r,c = row + dr, col + dc
+
+                    if r<0 or r >= rows or c <0 or c >= cols or grid[r][c] == "0" or (r,c) in visit:
+                        continue
+                    
+                    visit.add((r,c))
+                    q.append((r,c))
 
 
 
