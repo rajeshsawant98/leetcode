fe6ab8d7a2086e8d33e340c850fc8dbc83cf794c
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-07 18:52:04 -0700

    Time: 178 ms (76.64%) | Memory: 25.9 MB (34.16%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -2,13 +2,13 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         
         Baskets = {}
-        maxFruits = 0
+        output = 0
 
-        l= 0
+        l = 0
 
         for r in range(len(fruits)):
 
-            Baskets[fruits[r]] = 1 + Baskets.get(fruits[r],0)
+            Baskets[fruits[r]] = 1 + Baskets.get(fruits[r], 0)
 
             while len(Baskets) > 2:
 
@@ -17,13 +17,11 @@ class Solution:
                 if Baskets[fruits[l]] == 0:
                     del Baskets[fruits[l]]
                 
-                l += 1
+                l +=1
             
-            maxFruits = max(maxFruits, r - l + 1)
+            output = max(output, r - l + 1)
         
-
-        return maxFruits
-
+        return output
 
                 
             
