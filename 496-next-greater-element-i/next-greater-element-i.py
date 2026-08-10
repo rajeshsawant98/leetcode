8e7bc49ea8b3541800cf72f4de7b52050e0be791
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-09 19:05:00 -0700

    Time: 3 ms (40.25%) | Memory: 19.3 MB (92.53%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,27 +1,20 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
         nums1Map = { n:i for i,n in enumerate(nums1)}
-
         res = [-1] * len(nums1)
-
         stack = []
 
         for i in range(len(nums2)):
+            n = nums2[i]
 
-            curr = nums2[i]
-
-            while stack and curr > stack[-1]:
+            while stack and n > stack[-1]:
                 val = stack.pop()
-                index = nums1Map[val]
-                res[index] = curr
-            
-            if curr in nums1Map:
-                stack.append(curr)
-        
+                idx = nums1Map[val]
+                res[idx] = n
+            if n in nums1Map:
+                stack.append(n)
+
         return res
-
-
 
 
 
