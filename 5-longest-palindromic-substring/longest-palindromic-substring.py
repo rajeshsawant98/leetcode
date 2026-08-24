a0248f0acf6472ebb7adaecc221aefc743c33e6f
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-23 17:49:46 -0700

    Time: 299 ms (44.47%) | Memory: 19.3 MB (69.35%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,27 +1,35 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ""
-        resLen = 0
+        lenRes = 0
 
         for i in range(len(s)):
 
-            #odd substrings
-            
-            l,r = i,i
-            while l>= 0 and r< len(s) and s[l] == s[r]:
-                if (r-l+1) >resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l-=1
-                r+=1
-            
             #even substrings
-            l,r = i, i+1
-            while l>= 0 and r< len(s) and s[l] == s[r]:
-                if (r-l+1) >resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l-=1
-                r+=1
+            l = i 
+            r = i + 1
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    sublen = r - l + 1
+                    if sublen > lenRes:
+                        lenRes = sublen
+                        res = s[l:r+1]
+                    l-=1
+                    r+=1
+                else:
+                    break
+
+            #odd substrings
+            l=r= i
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    sublen = r - l + 1
+                    if sublen > lenRes:
+                        lenRes = sublen
+                        res = s[l:r+1]
+                    l-=1
+                    r+=1
+                else:
+                    break
         
         return res
